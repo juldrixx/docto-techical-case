2fb1748f7349504a6b2e8c598ed96e6fdb0888fb
Juldrixx <juldrixx@gmail.com>
2024-09-29 21:37:29 +0200

    Update ci

--- terraform.sh
+++ terraform.sh
@@ -1,6 +1,7 @@
 platform=""
 env=""
 cmd=""
+auto_approve="false"
 
 # FUNCTIONS
 usage() {
@@ -8,7 +9,8 @@ usage() {
   echo "usage : $0 --platform <ptf> --env <env> --cmd <cmd>" >&2
   echo "--platform aws or gcp" >&2
   echo "--env      terraform environment" >&2
-  echo "--cmd      terraform command to play"
+  echo "--cmd      terraform command to play" >&2
+  echo "--auto-approve  (optional) automatically approve terraform apply/destroy"
   exit 0
 }
 
@@ -31,6 +33,10 @@ while [ $# -gt 0 ]; do
     cmd=$2
     shift 2
     ;;
+  --auto-approve)
+    auto_approve="true"
+    shift 1
+    ;;
   -h | -help | --help) usage ;;
   *) break ;;
   esac
@@ -42,6 +48,11 @@ TF_IN_AUTOMATION=true TF_DATA_DIR="envs/${env}/.terraform" terraform -chdir="${p
 
 echo "$cmd terraform: "
 
+auto_approve_flag=""
+if [ "$auto_approve" = "true" ] && { [ "$cmd" = "apply" ] || [ "$cmd" = "destroy" ]; }; then
+  auto_approve_flag="-auto-approve"
+fi
+
 if [ $cmd = "output" ]; then
   echo "TF_DATA_DIR=\"envs/${env}/.terraform\" terraform \"${cmd}\""
   TF_DATA_DIR="envs/${env}/.terraform" terraform "${cmd}"
@@ -49,6 +60,6 @@ elif [ "$cmd" = "validate" ]; then
   echo "TF_DATA_DIR=\"envs/${env}/.terraform\" terraform -chdir=\"${platform}\" \"${cmd}\""
   TF_DATA_DIR="envs/${env}/.terraform" terraform -chdir="${platform}" "${cmd}"
 else
-  echo "TF_DATA_DIR=\"envs/${env}/.terraform\" terraform -chdir=\"${platform}\" \"${cmd}\" -var-file \"$(pwd)/${platform}/envs/${env}/config.tfvars\" -var-file \"$(pwd)/${platform}/envs/common.tfvars\""
-  TF_DATA_DIR="envs/${env}/.terraform" terraform -chdir="${platform}" "${cmd}" -var-file "$(pwd)/${platform}/envs/${env}/config.tfvars" -var-file "$(pwd)/${platform}/envs/common.tfvars"
+  echo "TF_DATA_DIR=\"envs/${env}/.terraform\" terraform -chdir=\"${platform}\" \"${cmd}\" -var-file \"$(pwd)/${platform}/envs/${env}/config.tfvars\" -var-file \"$(pwd)/${platform}/envs/common.tfvars\"  $auto_approve_flag"
+  TF_DATA_DIR="envs/${env}/.terraform" terraform -chdir="${platform}" "${cmd}" -var-file "$(pwd)/${platform}/envs/${env}/config.tfvars" -var-file "$(pwd)/${platform}/envs/common.tfvars  $auto_approve_flag"
 fi
