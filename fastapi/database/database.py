9d465caa43451aae5534d95cdf79a9454e33b3d3
Juldrixx <juldrixx@gmail.com>
2024-09-29 17:05:55 +0200

    WIP

--- fastapi/database/database.py
+++ fastapi/database/database.py
@@ -11,7 +11,7 @@ load_dotenv()
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT", 0)
+MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
 MYSQL_DB = os.getenv("MYSQL_DB")
 DB_URL = f"mysql+pymysql://{MYSQL_USER}:{
     MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
