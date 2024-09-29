371de6213a5a79520b9b788bc26043283848e2b2
Juldrixx <juldrixx@gmail.com>
2024-09-29 23:27:59 +0200

    Fix typo

--- aws/vpc/iam.tf
+++ aws/vpc/iam.tf
@@ -17,5 +17,5 @@ resource "aws_iam_role" "vpc" {
 
 resource "aws_iam_role_policy_attachment" "flow_logs_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_VPC_FlowLogs"
-  role       = aws_iam_role.vpc2.name
+  role       = aws_iam_role.vpc.name
 }
