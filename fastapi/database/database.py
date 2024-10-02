79c973a5c6e82cac3c229bcfb901006459238895
Juldrixx <juldrixx@gmail.com>
2024-10-02 10:14:41 +0200

    Add default

--- fastapi/database/database.py
+++ fastapi/database/database.py
@@ -9,11 +9,11 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = quote(os.getenv("MYSQL_PASSWORD"))
-MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER", "DEFAULT_USER")
+MYSQL_PASSWORD = quote(os.getenv("MYSQL_PASSWORD", "DEFAULT_PASS"))
+MYSQL_HOST = os.getenv("MYSQL_HOST", "DEFAULT_HOST")
 MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_DB = os.getenv("MYSQL_DB", "DEFAULT_DB")
 DB_URL = f"mysql+pymysql://{MYSQL_USER}:{
     MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
